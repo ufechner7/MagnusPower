462615f17a757ced28c5d1feec6e14b870383fd0
Uwe Fechner <u.fechner-1@tudelft.nl>
2024-05-16 20:26:46 +0200

    add calc_force

--- src/magnus.jl
+++ src/magnus.jl
@@ -1,6 +1,9 @@
 using ControlPlots
 # Re = 3.8e4
 
+const RHO = 1.225  # air density at zero height and 15 °C    [kg/m³]
+const AREA = 0.8   # area of the cylinder in m²
+
 # x: speed ratio
 # x = ωR/v_a
 function cd(x)
@@ -10,6 +13,15 @@ end
 # x: speed ratio
 function cl(x)
     0.0126 * x^4 - 0.2004*x^3 + 0.7482*x^2 + 1.3447*x
+end
+
+function calc_force(elev, x, v_wind::Float64)
+    v_app = v_wind
+    CL = cl(x) 
+    CD = cd(x)
+    # L = (-0.5 * RHO * (norm(va_xz2))^2 * AREA * CL) * normalize(va_2 × y)
+    # D = (-0.5 * RHO * norm(va_2) * AREA * CD) * va_2
+
 end
 
 X=0.0:0.1:5.5
