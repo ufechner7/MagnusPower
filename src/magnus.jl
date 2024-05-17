6487f8f492139d060a7f832d8ad2c1f473c6b600
Uwe Fechner <fechner@aenarete.eu>
2024-05-17 13:24:43 +0200

    Update magnus.jl

--- src/magnus.jl
+++ src/magnus.jl
@@ -1,4 +1,4 @@
-using ControlPlots
+using ControlPlots, LinearAlgebra
 # Re = 3.8e4
 
 const RHO = 1.225  # air density at zero height and 15 °C    [kg/m³]
@@ -15,16 +15,17 @@ function cl(x)
     0.0126 * x^4 - 0.2004*x^3 + 0.7482*x^2 + 1.3447*x
 end
 
+# x: speed ratio
 function calc_force(x, v_wind::Float64)
-    v_app = v_wind
+    v_a = v_wind
     CL = cl(x) 
     CD = cd(x)
-    L = -0.5 * RHO * v_a^2 * AREA * CL
-    D = -0.5 * RHO * va^2 * AREA * CD
-    [L, D]
+    L = 0.5 * RHO * v_a^2 * AREA * CL
+    D = 0.5 * RHO * v_a^2 * AREA * CD
+    norm([L, D]), rad2deg(atan(L, D))
 end
 
-X=0.0:0.1:5.5
+X = 0.0:0.1:5.5
 CL = cl.(X)
 CD = cd.(X)
 LoD = CL ./ CD
