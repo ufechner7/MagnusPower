b4e0295b7f44f262980f153434abd112b542ec9e
Uwe Fechner <u.fechner-1@tudelft.nl>
2024-05-22 16:24:18 +0200

    add model.jl

---
+++ src/model.jl
@@ -0,0 +1,24 @@
+using ModelingToolkit, OrdinaryDiffEq
+
+mutable struct Model
+    sys
+end
+
+const elev0 = 70.0 # initial elevation angle in degrees
+const L0    = 10.0 # initial tether length
+const POS0  =   L0*[cos(deg2rad(elev0)), sin(deg2rad(elev0))]
+const VEL0  = zeros(2)
+const ACC0  = zeros(2)
+function model2D()
+    @variables t
+    @variables pos(t)[1:2]  = POS0
+    @variables vel(t)[1:2]  = VEL0
+    @variables acc(t)[1:2]  = ACC0
+    D = Differential(t)
+    eqs=[
+        D(pos) ~ vel,
+        D(vel) ~ acc
+    ]
+    @named sys = ODESystem(eqs, t)
+    Model(sys)
+end
